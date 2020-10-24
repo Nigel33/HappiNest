70d2b00acc9c6ddd74f1a903929e7bb86b08daeb
Nigel Hing <nigelhing1@gmail.com>
2020-10-24 22:43:02 +0800

    add staticQuery

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,15 +1,42 @@
 import React from "react";
 import { MDBCarousel, MDBCarouselInner, MDBCarouselItem, MDBView, MDBMask, MDBBtn } from
     "mdbreact";
+import { useStaticQuery, graphql } from "gatsby"
+import Img from 'gatsby-image'
 import heroImage from "../images/hero.png"
 
 
+
+
 const CarouselPage = () => {
+  const data = useStaticQuery(graphql`
+  query{
+    image1: 
+      file(
+        relativePath: { eq: "hero.png"}      
+      ) {
+        childImageSharp {
+          fluid(maxWidth: 1200) {
+            base64
+            aspectRatio
+            src
+            srcSet
+            sizes
+          }
+        }
+      }   
+    } 
+    `)
+  
     return (
       <>
         <MDBView>
           <div className="position-relative"> 
-            <div className="hero-box"></div>
+            <div className="hero-box">
+              <Img fluid={data.image1.childImageSharp.fluid}
+                alt="test" />   
+            </div>
+
             {/* <MDBMask overlay="black-slight" /> */}
             {/* <MDBBtn className="hero-cta-button full-width test test-2 test-3">Buy Now</MDBBtn> */}
           </div>          
